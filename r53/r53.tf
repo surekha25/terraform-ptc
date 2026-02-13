a0a48cab9380482bac933254f3c1146ea1033658
surekha25 <surekhasuri25@gmail.com>
2026-02-13 10:52:21 +0530

    terraform

---
+++ r53/r53.tf
@@ -0,0 +1,9 @@
+resource "aws_route53_record" "roboshop" {
+  count = 3
+  zone_id = var.zone_id
+  name    = "${var.instances[count.index]}.${var.domain_name}" #mangodb.surekhadevops.biz
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.terraform[count.index].private_ip]
+  allow_overwrite = true
+}
