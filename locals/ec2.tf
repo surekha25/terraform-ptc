97e05a7a5810bbbbb7a9ab05c2c77b2f887b8154
surekha25 <surekhasuri25@gmail.com>
2026-02-16 11:43:59 +0530

    terraform

---
+++ locals/ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "terraform" {
+  ami           = var.ami_id
+  instance_type = local.instance_type
+  vpc_security_group_ids = [aws_security_group.terraform_allow_ports.id]
+
+  tags = var.ec2_tags
+}
+
+resource "aws_security_group" "terraform_allow_ports" {
+  # ... other configuration ...
+  name = var.sg_name
+
+  egress {
+    from_port        = var.egress_from_port
+    to_port          = var.egress_to_port
+    protocol         = var.egress_protocol
+    cidr_blocks      = var.cidr
+  }
+
+  ingress {
+    from_port        = var.ingress_from_port
+    to_port          = var.ingress_to_port
+    protocol         = var.ingress_protocol
+    cidr_blocks      = var.cidr
+  }
+
+  tags = {
+    Name = "Terraform Allow All Ports"
+  }
+}
